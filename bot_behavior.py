ad033e7e3c20c65775931a01afc5e7862061b030
mcmoots <criminy.crickets@gmail.com>
2014-06-22 18:57:06 -0700

    Classes, methods, and sockets, like a grownup.

--- bot_behavior.py
+++ bot_behavior.py
@@ -4,71 +4,98 @@ import random
 import sys
 import swifties
 from wordnik import *
+import logging
+import socket
+from time import sleep
 
 
-# define project root
-if len(sys.argv) > 1:
-    rootdir = sys.argv[1]
-else:
-    rootdir = './'
+class BotSwifty:
+
+    def __init__(self, rootdir):
+
+        self.last_tweet_file = rootdir + "last_tweet.txt"
+
+        confs = yaml.load(open(rootdir + 'config.yaml'))
+        self.api = twitter.Api(**confs['twitter_tokens'])
+
+        wordnik_url = 'http://api.wordnik.com/v4'
+        wordnik_key = confs['wordnik_key']
+        self.client = swagger.ApiClient(wordnik_key, wordnik_url)
+        self.wordnik = WordApi.WordApi(self.client)
+        swifties.wordnik = self.wordnik
+
+    def run_bot(self):
+
+        # Follow/unfollow
+        followers = set([fol.id for fol in self.api.GetFollowers()])
+        following = set([fol.id for fol in self.api.GetFriends()])
+
+        to_follow = followers - following   # followers who are not yet followed
+        to_unfollow = following - followers  # users who are no longer following back
+
+        for follow in to_follow:
+            self.api.CreateFriendship(user_id=follow)
+
+        for unfollow in to_unfollow:
+            self.api.DestroyFriendship(user_id=unfollow)
+
+        # read in the last seen tweet
+        f = open(self.last_tweet_file, "r")
+        last_tweet = f.read()
+        f.close()
+
+        # Pull tweets from bot's home timeline
+        tweets_full = self.api.GetHomeTimeline(exclude_replies=True, since_id=last_tweet)
+        tweets = [tweet for tweet in tweets_full if tweet.retweeted_status is None]
+
+        if len(tweets) == 0:
+            sys.exit()
+
+        authors = set([tweet.user.screen_name for tweet in tweets])
+
+        tries = 0
+        while tries < 4:
+            author = random.sample(authors, 1)[0]
+            tweet = random.choice([tweet for tweet in tweets if tweet.user.screen_name == author])
+
+            s = swifties.Swifties(rootdir)
+            reply_text = s.makeSwifty(tweet)
+            if None == reply_text:
+                tries += 1
+                continue
+
+            try:
+                self.api.PostUpdate(reply_text, in_reply_to_status_id=tweet.id)
+                break
+            except twitter.TwitterError:
+                tries += 1
+
+        #store most recently seen tweet id
+        f = open(self.last_tweet_file, "w")
+        f.write(tweets_full[0].id_str)
+        f.close()
+
+        return True
 
 
-last_tweet_file = rootdir + "last_tweet.txt"
-
-confs = yaml.load(open(rootdir + 'config.yaml'))
-api = twitter.Api(**confs['twitter_tokens'])
-
-wordnik_url = 'http://api.wordnik.com/v4'
-wordnik_key = confs['wordnik_key']
-client = swagger.ApiClient(wordnik_key, wordnik_url)
-wordnik = WordApi.WordApi(client)
-swifties.wordnik = wordnik
-
-# Follow/unfollow
-
-followers = set([fol.id for fol in api.GetFollowers()])
-following = set([fol.id for fol in api.GetFriends()])
-
-to_follow = followers - following   # followers who are not yet followed
-to_unfollow = following - followers  # users who are no longer following back
-
-for follow in to_follow:
-    api.CreateFriendship(user_id=follow)
-
-for unfollow in to_unfollow:
-    api.DestroyFriendship(user_id=unfollow)
-
-# read in the last seen tweet
-f = open(last_tweet_file, "r")
-last_tweet = f.read()
-f.close()
-
-# Pull tweets from bot's home timeline
-tweets_full = api.GetHomeTimeline(exclude_replies=True, since_id=last_tweet)
-tweets = [tweet for tweet in tweets_full if tweet.retweeted_status is None]
-
-if len(tweets) == 0:
-    sys.exit()
-
-authors = set([tweet.user.screen_name for tweet in tweets])
-
-tries = 0
-while tries < 4:
-    author = random.sample(authors, 1)[0]
-    tweet = random.choice([tweet for tweet in tweets if tweet.user.screen_name == author])
-    reply_text = swifties.pull_sentence_from_tweet(tweet)
-    if None == reply_text:
-        tries += 1
-        continue
-
+if __name__ == '__main__':
+    lock_socket = socket.socket(socket.AF_UNIX, socket.SOCK_DGRAM)
+    lock_id = "mcmoots.botswifty"
     try:
-        api.PostUpdate(reply_text, in_reply_to_status_id=tweet.id)
-        break
-    except twitter.TwitterError:
-        tries += 1
+        lock_socket.bind('\0' + lock_id)
+        logging.debug("Got lock %r" % (lock_id,))
+    except socket.error:
+        # socket already locked, task must be running
+        logging.info("Failed to get lock %r" % (lock_id,))
+        sys.exit()
 
+    if len(sys.argv) > 1:
+        rootdir = sys.argv[1]
+    else:
+        rootdir = './'
 
-#store most recently seen tweet id
-f = open(last_tweet_file, "w")
-f.write(tweets_full[0].id_str)
-f.close()
+    b = BotSwifty(rootdir)
+
+    while True:
+        b.run_bot()
+        sleep(10987)
--- swifties.py
+++ swifties.py
@@ -1,107 +1,110 @@
 # take a sentence, attempt to make a Tom Swifty pun
-import sys
+
 import random
-
-
 from nltk.corpus import stopwords
 from nltk.corpus import wordnet as wn
-
-
-# set up stemmer
 from nltk.stem.porter import *
-
-stemmer = PorterStemmer()
 import nltk.data
-sentence_detector = nltk.data.load('tokenizers/punkt/english.pickle')
 
-# define project root
-if len(sys.argv) > 1:
-    rootdir = sys.argv[1]
-else:
-    rootdir = './'
+class Swifties:
+    def __init__(self, rootdir):
+        # set up stemmer
+        self.stemmer = PorterStemmer()
+        self.sentence_detector = nltk.data.load('tokenizers/punkt/english.pickle')
 
-# load dictionary
-sowpods = set(line.strip() for line in open(rootdir + 'sowpods.txt'))
-ends_in_ly = re.compile('ly$')
-sowpodsly = filter(ends_in_ly.search, sowpods)
+        self.stops = stopwords.words('english')
+        self.stops += ['are', 'out', 'guy', 'was', 'so', 'is', 'in', 'do', 'thou']
 
-# WordNet stop list
-# http://www.d.umn.edu/~tpederse/Group01/WordNet/wordnet-stoplist.html
-stops = stopwords.words('english')
-stops += ['are', 'out', 'guy', 'was', 'so', 'is', 'in', 'do', 'thou']
+        sowpods = set(line.strip() for line in open(rootdir + 'sowpods.txt'))
+        ends_in_ly = re.compile('ly$')
+        self.sowpodsly = filter(ends_in_ly.search, sowpods)
 
-def find_ly_adverb(word):
-    """Take a word and return an -ly adverb for it
 
-    :rtype : basestring
-    :param word:
+    def findLy(self, word):
+        """Take a word and return an -ly adverb for it
+
+        :rtype : basestring
+        :param word:
+        """
+        wstem = self.stemmer.stem(word)
+        # todo pull prefixes out into list for config section
+        prefixes = '(un|out|in|up|pro|con|anti|pre|ante|en|non|re|a|co|ex|dis|de|mal|mis|over|under|post)'
+        suffixes = '(ish|ous|eous|ful|l|il|i|ab|al|ib|less)'
+        re_string = prefixes + '*' + wstem + suffixes + '*ly$'
+        re_ly = re.compile(re_string)
+        advs = filter(re_ly.match, self.sowpodsly)
+
+        return advs
+
+
+    def findSyns(self, word, explain=False):
+        'Take a word, get synonyms, check each for an -ly adverb'
+
+        synos = wordnik.getRelatedWords(word, useCanonical=True, relationshipTypes='synonym')
+
+        if synos == None:
+            return []
+
+        syns = synos[0].words
+        adverbs = []
+
+        for syn in syns:
+            adverbs += self.findLy(syn)
+
+        return adverbs
+
+
+    def swiftifyString(self, tweet, handle, maxlength):
+        """ Take a string + Twitter handle, build Swifty pun that stays within length limits
+
+        Step 1: Generate list of all synonym-y adverbs for all words
+        Step 2: Pick an adverb that stays within length limit
+        Step 3: Assemble & return string
+        """
+
+        # split string into words - does NLTK have a better thing for this?
+        words = [wn.morphy(word.lower()) for word in tweet.split()]
+        words = [w for w in words if w is not None]
+        adverbs = []
+
+        # todo parse parts of speech, send only non-verbs/non-adverbs?
+        for word in words:
+            if len(word) > 2 and word.lower() not in self.stops:
+                adverbs += self.findSyns(word.lower(), False)
+
+        if len(adverbs) == 0:
+            return None
+
+        # string: "tweet", said @handle adverbly.
+        # length: tweet length + handle length + 12
+        max_adv_length = maxlength - 13 - len(tweet) - len(handle)
+        adverbly = random.choice([a for a in adverbs if len(a) <= max_adv_length])
+
+        tweetstring = '"' + tweet + '", said @' + handle + ' ' + adverbly + '.'
+        return tweetstring
+
+
+    def makeSwifty(self, tweet):
+        sentences = self.sentence_detector.tokenize(tweet.text)
+        replies = []
+        for s in sentences:
+            t = clean_tweet(s)
+            replies.append( self.swiftifyString(t, tweet.user.screen_name, 140) )
+
+        replies = filter(None, replies)
+
+        if [None] != replies and len(replies) > 0:
+            return random.choice(replies)
+        else:
+            return None
+
+
+def clean_tweet(tweet):
+    """ Strip URLs and hashtags
+    :param tweet:
+    :return:
     """
-    wstem = stemmer.stem(word)
-    # todo pull prefixes out into list for config section
-    prefixes = '(un|out|in|up|pro|con|anti|pre|ante|en|non|re|a|co|ex|dis|de|mal|mis|over|under|post)'
-    suffixes = '(ish|ous|eous|ful|l|il|i|ab|al|ib|less)'
-    re_string = prefixes + '*' + wstem + suffixes + '*ly$'
-    re_ly = re.compile(re_string)
-    advs = filter(re_ly.match, sowpodsly)
-
-    return advs
-
-
-def find_syns(word, explain=False):
-    'Take a word, get synonyms, check each for an -ly adverb'
-
-    synos = wordnik.getRelatedWords(word, useCanonical=True, relationshipTypes='synonym')
-
-    if synos == None:
-        return []
-
-    syns = synos[0].words
-    adverbs = []
-
-    for syn in syns:
-        adverbs += find_ly_adverb(syn)
-
-    return adverbs
-
-def swiftify_string(tweet, handle, maxlength):
-    """ Take a string + Twitter handle, build Swifty pun that stays within length limits
-
-    Step 1: Generate list of all synonym-y adverbs for all words
-    Step 2: Pick an adverb that stays within length limit
-    Step 3: Assemble & return string
-    """
-
-    # split string into words - does NLTK have a better thing for this?
-    words = [wn.morphy(word.lower()) for word in tweet.split()]
-    words = [w for w in words if w is not None]
-    adverbs = []
-
-    # todo parse parts of speech, send only non-verbs/non-adverbs?
-    for word in words:
-        if len(word) > 2 and word.lower() not in stops:
-            adverbs += find_syns(word.lower(), False)
-
-    if len(adverbs) == 0:
-        return None
-
-    # string: "tweet", said @handle adverbly.
-    # length: tweet length + handle length + 12
-    max_adv_length = maxlength - 13 - len(tweet) - len(handle)
-    adverbly = random.choice([a for a in adverbs if len(a) <= max_adv_length])
-
-    tweetstring = '"' + tweet + '", said @' + handle + ' ' + adverbly + '.'
-    return tweetstring
-
-#todo: rename this function
-#todo: stop spitting out errors when swiftify_string returns a blank (replies = [None])
-def pull_sentence_from_tweet(tweet):
-    sentences = sentence_detector.tokenize(tweet.text)
-    replies = []
-    for s in sentences:
-        # todo clean hashtags, urls, @-replies, etc
-        replies.append( swiftify_string(s, tweet.user.screen_name, 140) )
-    replies = filter(None, replies)
-    if [None] != replies:
-        return random.choice(replies)
-    else:
-        return None
+    urls = re.compile('https?:\/\/\S+', re.IGNORECASE)
+    tweet = re.sub(urls, '', tweet)
+    tweet = re.sub(r'#', '', tweet)
+    return tweet
